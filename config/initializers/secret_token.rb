b18764a0f3c6d81790c2d25ea4b6bd810e924c70
Meng Fung <meng.fung@gmail.com>
2012-09-18 09:12:01 -0700

    initialize rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DogbrigadeV2::Application.config.secret_token = 'af38611603a5808761c3ec8330ea51dab77bbaee130f6232630d2e61ad80b7c1c3d6873223e41fb915a5f0c86bb94dd2944abc7bc1f5999450dbbf766b8e5476'
