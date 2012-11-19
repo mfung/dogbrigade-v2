eade1dc75af5c4f6c6fe8cf831f3cd5b7dbe3ff3
Meng Fung <meng.fung@gmail.com>
2012-11-18 19:44:17 -0800

    added password_digest to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121119014208) do
+ActiveRecord::Schema.define(:version => 20121119034351) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "fname"
     t.string   "lname"
     t.string   "zip_code"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
