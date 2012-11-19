0f24288a558f734971a6a3942cfe4afa2bd8c025
Meng Fung <meng.fung@gmail.com>
2012-11-18 19:49:48 -0800

    added attr_accessible for password and password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :fname, :lname, :zip_code
+  has_secure_password
+  attr_accessible :email, :fname, :lname, :zip_code, :password, :password_confirmation
   
   validates :email, presence: true, format: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, uniqueness: true
   validates :fname, presence: true
