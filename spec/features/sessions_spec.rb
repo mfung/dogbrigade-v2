e622448b18e32c66cbafaf778dce4cd2b9877a9e
Meng Fung <meng.fung@gmail.com>
2012-12-05 19:21:30 -0800

    adding login test

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-feature 'User Management', %q{
+feature 'Sessions Management', %q{
   As a dog lover
   In order to make it easier to search for dogs
   I need to be able to login and loggout
@@ -10,6 +10,14 @@ feature 'User Management', %q{
         visit login_path
         expect(page).to have_content('Sign In')
         expect(page).to have_selector('#login_email')
+      end
+      
+      it 'logs in a user' do
+        visit login_path
+        expect {
+          fill_in '', ''
+          click 'Login'
+        }.to have_content('Signed In.')
       end
     end 
   end
