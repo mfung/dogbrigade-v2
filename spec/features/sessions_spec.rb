10782d9e16ce728c1354be9e07c74803e782a612
Meng Fung <meng.fung@gmail.com>
2012-12-05 22:10:54 -0800

    added login spec

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -6,6 +6,10 @@ feature 'Sessions Management', %q{
 } do
   context 'guest' do
     describe 'Sessions#new' do
+      before :each do
+        FactoryGirl.create(:user, email: 'test@mail.com', password: 'secret', password_confirmation: 'secret')
+      end
+      
       it 'has a login form' do
         visit login_path
         expect(page).to have_content('Sign In')
@@ -14,10 +18,22 @@ feature 'Sessions Management', %q{
       
       it 'logs in a user' do
         visit login_path
-        expect {
-          fill_in '', ''
-          click 'Login'
-        }.to have_content('Signed In.')
+        within '#login-form' do
+          fill_in 'login[email]', with: 'test@mail.com'
+          fill_in 'login[password]', with: 'secret'
+          click_button 'Login'
+        end
+        expect(page).to have_content('Logged in!')
+      end
+      
+      it 'does not login a user if invalid' do
+        visit login_path
+        within '#login-form' do
+          fill_in 'login[email]', with: 'test@mail.com'
+          fill_in 'login[password]', with: 'secret2'
+          click_button 'Login'
+        end
+        expect(page).to have_content('Email or password is invalid!')
       end
     end 
   end
