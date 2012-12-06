fb27f72ca9ece1ac1280ff51acb1ce2ed24186b0
Meng Fung <meng.fung@gmail.com>
2012-12-05 18:28:46 -0800

    configure login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DogbrigadeV2::Application.routes.draw do
+  get "sessions/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -15,6 +17,8 @@ DogbrigadeV2::Application.routes.draw do
   
   match 'about_us' => 'pages#about_us'
   resources :users
+  match 'login' => 'sessions#new'
+  match 'logout' => 'sessions#destory'
   
   # Sample resource route with options:
   #   resources :products do
