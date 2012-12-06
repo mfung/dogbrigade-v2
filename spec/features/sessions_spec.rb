29558ed3e8d579a55aa7fe2c85023a7cccaf71e4
Meng Fung <meng.fung@gmail.com>
2012-12-05 18:30:22 -0800

    added sessions spec

---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+feature 'User Management', %q{
+  As a dog lover
+  In order to make it easier to search for dogs
+  I need to be able to login and loggout
+} do
+  context 'guest' do
+    describe 'Sessions#new' do
+      it 'has a login form' do
+        visit login_path
+        expect(page).to have_content('Sign In')
+        expect(page).to have_selector('form#login #username')
+      end
+    end 
+  end
+end
