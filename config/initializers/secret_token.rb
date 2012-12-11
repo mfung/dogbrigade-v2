c0f59d087493dfef52b38731d96d7015626c834f
Meng Fung <meng.fung@gmail.com>
2012-12-10 19:41:27 -0800

    added application.yml to store secret tokens for web app and petfinder

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DogbrigadeV2::Application.config.secret_token = 'af38611603a5808761c3ec8330ea51dab77bbaee130f6232630d2e61ad80b7c1c3d6873223e41fb915a5f0c86bb94dd2944abc7bc1f5999450dbbf766b8e5476'
+DogbrigadeV2::Application.config.secret_token = CONFIG[:secret_token]
