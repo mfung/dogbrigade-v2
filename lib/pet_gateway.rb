82f898dd18140124be48529155ce60d3e47f767f
Meng Fung <meng.fung@gmail.com>
2012-12-10 23:17:53 -0800

    finished gateway

--- lib/pet_gateway.rb
+++ lib/pet_gateway.rb
@@ -3,6 +3,7 @@ class PetGateway
     @petfinder ||= Petfinder::Client.new
   end
   
+  # options animal_type: location: breed: size: sex: location: shelterid:
   def find(options = {})
     defaults = {:animal_type => 'dog', :location => '90210'}
     options = defaults.merge(options)
@@ -26,10 +27,27 @@ class PetGateway
     @petfinder.shelter(id)
   end
   
+  # options location: name: offset: count:
   def find_shelters(options = {})
     defaults = {:location => '90210'}
     options = defaults.merge(options)
     
     @petfinder.find_shelters(options[:location], options)
+  end
+  
+  # options animal_type: breed: offset: count:
+  def find_shelters_by_breed(options = {})
+    defaults = {:animal_type => 'dog', :breed => 'German Shepherd Dog'}
+    options = defaults.merge(options)
+    
+    @petfinder.find_shelters_by_breed(options[:animal_type], options[:breed])
+  end
+  
+  # options id: status: offset: count:
+  def shelter_pets(options = {})
+    defaults = {:id => 1}
+    options = defaults.merge(options)
+    
+    @petfinder.shelter_pets(options[:id], options)
   end
 end
