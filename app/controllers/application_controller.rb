599b0541c7f50e4a6f72dcb5c869cf17b2a07ec1
Meng Fung <meng.fung@gmail.com>
2013-04-01 17:21:48 -0700

    added more information to dog detail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  STATUS = {'A'=>'Adoptable', 'H'=>'Hold', 'P'=>'Pending', 'X'=>'Adopted/Removed'}
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,4 @@
 SEX = {'F' => 'Female', 'M' => 'Male'}
 STATUS = {'A' => 'Adoptable', 'H' => 'Hold', 'P' => 'Pending', 'X' => "Adopted / Removed"}
+AGE = ['Baby', 'Young', 'Adult', 'Senior']
+SIZE = {'S' => 'Small', 'M' => 'Medium', 'L' => 'Large', 'XL' => 'Extra Large'}
